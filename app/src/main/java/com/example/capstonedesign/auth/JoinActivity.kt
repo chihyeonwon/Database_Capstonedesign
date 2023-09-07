3e1e01afa0ab3a3302c93fb8c806e1525b1b9e61
ylose <heungeob1003@gmail.com>
2023-09-07 22:11:49 +0900

    회원가입 예시 코드 실행 23.09.07

--- app/src/main/java/com/example/capstonedesign/auth/JoinActivity.kt
+++ app/src/main/java/com/example/capstonedesign/auth/JoinActivity.kt
@@ -2,11 +2,27 @@ package com.example.capstonedesign.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.capstonedesign.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+        auth = FirebaseAuth.getInstance()
+
+        auth.createUserWithEmailAndPassword("abc@abc.com", "abcdabcd")
+            .addOnCompleteListener(this) { task ->
+                if(task.isSuccessful) {
+                    Toast.makeText(this,"성공", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this,"실패",Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
