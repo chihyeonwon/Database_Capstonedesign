96d20712c016b63a22e0e87fdb65efdbc9d4335b
ylose <heungeob1003@gmail.com>
2023-09-07 16:53:09 +0900

    LoginActivity, JoinActivity 생성 23.09.07

---
+++ app/src/main/java/com/example/capstonedesign/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.capstonedesign.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstonedesign.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
