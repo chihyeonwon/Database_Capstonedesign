62410266feaaf946864f2503bd8b9efd2b2aff0a
ylose <heungeob1003@gmail.com>
2023-09-07 16:54:03 +0900

    데이터 바인딩 binding 생성23.09.07

--- app/src/main/java/com/example/capstonedesign/auth/IntroActivity.kt
+++ app/src/main/java/com/example/capstonedesign/auth/IntroActivity.kt
@@ -2,11 +2,17 @@ package com.example.capstonedesign.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.capstonedesign.R
+import com.example.capstonedesign.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
     }
 }
