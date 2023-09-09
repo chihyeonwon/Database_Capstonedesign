faf2fa774e8de97ef291dc59e0231db6319ebbb2
ylose <heungeob1003@gmail.com>
2023-09-09 18:54:05 +0900

    LoginActivity 바인딩 생성 23.09.09

--- app/src/main/java/com/example/capstonedesign/auth/LoginActivity.kt
+++ app/src/main/java/com/example/capstonedesign/auth/LoginActivity.kt
@@ -2,11 +2,17 @@ package com.example.capstonedesign.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.capstonedesign.R
+import com.example.capstonedesign.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
     }
 }
