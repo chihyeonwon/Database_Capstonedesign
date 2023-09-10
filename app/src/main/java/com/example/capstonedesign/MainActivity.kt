1b49ea39c5056ba4f7d30420dccd34ab8ddb09f6
ylose <heungeob1003@gmail.com>
2023-09-10 15:38:02 +0900

    버튼을 눌렀을 때 로그아웃 기능 구현 23.09.10

--- app/src/main/java/com/example/capstonedesign/MainActivity.kt
+++ app/src/main/java/com/example/capstonedesign/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.capstonedesign
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = FirebaseAuth.getInstance()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.logoutBtn).setOnClickListener {
+
+            // 로그아웃
+            auth.signOut()
+
+        }
     }
 }
