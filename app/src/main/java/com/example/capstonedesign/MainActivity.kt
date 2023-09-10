7c49720963529b9a929434ec6d68db3fa3d3b347
ylose <heungeob1003@gmail.com>
2023-09-10 15:45:03 +0900

    로그아웃 버튼을 눌렀을 때 IntroActivity로 화면 이동 기능 구현 23.09.10

--- app/src/main/java/com/example/capstonedesign/MainActivity.kt
+++ app/src/main/java/com/example/capstonedesign/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.capstonedesign
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.capstonedesign.auth.IntroActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +25,10 @@ class MainActivity : AppCompatActivity() {
             // 로그아웃
             auth.signOut()
 
+            // IntroActivity로 화면 이동
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
         }
     }
 }
