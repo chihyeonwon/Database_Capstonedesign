dab1d22292eadc22e0d2ba2e8cfe8cbc98259bee
ylose <heungeob1003@gmail.com>
2023-09-10 15:52:48 +0900

    익명로그인 기능 및 MainActivity로의 화면 이동 기능 구현 23.09.10

--- app/src/main/java/com/example/capstonedesign/auth/IntroActivity.kt
+++ app/src/main/java/com/example/capstonedesign/auth/IntroActivity.kt
@@ -3,16 +3,24 @@ package com.example.capstonedesign.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import com.example.capstonedesign.MainActivity
 import com.example.capstonedesign.R
 import com.example.capstonedesign.databinding.ActivityIntroBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthActionCodeException
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     private lateinit var binding : ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
 
@@ -24,6 +32,23 @@ class IntroActivity : AppCompatActivity() {
         binding.joinBtn.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.noAccountBtn.setOnClickListener {
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful) {
+
+                        Toast.makeText(this,"로그인 성공", Toast.LENGTH_LONG).show()
+
+                        // MainAcitivity로 화면 이동
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this,"로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
         }
     }
 }
