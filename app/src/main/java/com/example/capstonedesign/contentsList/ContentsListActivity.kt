fb5418542b281e75ea2324b77e5cc3929569c247
ylose <heungeob1003@gmail.com>
2023-09-18 16:06:45 +0900

    RVAdapter를 연결하고 컨텐츠를 넣는 작업 23.09.18

---
+++ app/src/main/java/com/example/capstonedesign/contentsList/ContentsListActivity.kt
@@ -0,0 +1,30 @@
+package com.example.capstonedesign.contentsList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.capstonedesign.R
+
+class ContentsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contents_list)
+
+        // activity_contents_list에서 생성한 RecycleView를 가져온다.
+        val rv : RecyclerView = findViewById(R.id.rv)
+
+        // 아이템들을 넣는다.
+        val items = ArrayList<String>()
+        items.add("a")
+        items.add("b")
+        items.add("c")
+
+        // Activity에 있는 rv의 adavpter를 가져와서 연결한다.
+        val rvAdapter = ContentRVAdapter(items)
+        rv.adapter = rvAdapter
+
+        rv.layoutManager = LinearLayoutManager(this)
+    }
+}
