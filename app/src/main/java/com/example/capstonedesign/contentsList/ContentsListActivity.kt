1fdfc20109a831f5654ad97c029f6897711dfeed
ylose <heungeob1003@gmail.com>
2023-09-24 18:14:09 +0900

    데이터베이스의 데이터를 불러오기 23.09.23

--- app/src/main/java/com/example/capstonedesign/contentsList/ContentsListActivity.kt
+++ app/src/main/java/com/example/capstonedesign/contentsList/ContentsListActivity.kt
@@ -3,6 +3,7 @@ package com.example.capstonedesign.contentsList
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.Toast
@@ -10,6 +11,9 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.capstonedesign.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -18,26 +22,64 @@ class ContentsListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contents_list)
 
+        // 아이템들을 넣는다.
+        val items = ArrayList<ContentModel>()
+
+        // Activity에 있는 rv의 adavpter를 가져와서 연결한다.
+        val rvAdapter = ContentRVAdapter(baseContext, items)
+
         // Write a message to the database
         val database = Firebase.database
         val myRef = database.getReference("contents")
 
-       /* myRef.push()
+        // Firebase Database의 데이터 읽기
+        val postListner = object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // dataModel에 있는 데이터를 하나씩 가져오는 부분
+                for(dataModel in dataSnapshot.children) {
+                    Log.d("ContentListActivity",dataSnapshot.toString())
+
+                    // 가져온 데이터를 컨텐츠 모델 형태로 받는 부분
+                    val item = dataModel.getValue(ContentModel::class.java)
+                    items.add(item!!)
+                }
+                // 데이터를 받고 나서 어뎁터 동기화하는 부분
+                rvAdapter.notifyDataSetChanged()
+                Log.d("ContentListActivity",items.toString())
+            }
+             
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("ContentListActivity", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        myRef.addValueEventListener(postListner)
+
+        // activity_contents_list에서 생성한 RecycleView를 가져온다.
+        val rv : RecyclerView = findViewById(R.id.rv)
+
+        rv.adapter = rvAdapter
+
+        rv.layoutManager = GridLayoutManager(this, 2)
+
+        rvAdapter.itemClick = object: ContentRVAdapter.ItemClick{
+            override fun onClick(view: View, position: Int) {
+
+                val intent = Intent(this@ContentsListActivity,ContentShowActivity::class.java)
+                intent.putExtra("url",items[position].webUrl)
+                startActivity(intent)
+
+            }
+        }
+        /*myRef.push()
             .setValue(ContentModel("title1","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png","https://philosopher-chan.tistory.com/1235"))
         myRef.push()
             .setValue(ContentModel("title2","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FznKK4%2Fbtq665AUWem%2FRUawPn5Wwb4cQ8BetEwN40%2Fimg.png","https://philosopher-chan.tistory.com/1236"))
         myRef.push()
             .setValue(ContentModel("title3","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbtig9C%2Fbtq65UGxyWI%2FPRBIGUKJ4rjMkI7KTGrxtK%2Fimg.png","https://philosopher-chan.tistory.com/1237"))
         myRef.push()
-            .setValue(ContentModel("title4","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcOYyBM%2Fbtq67Or43WW%2F17lZ3tKajnNwGPSCLtfnE1%2Fimg.png","https://philosopher-chan.tistory.com/1238"))*/
-
-
-        // activity_contents_list에서 생성한 RecycleView를 가져온다.
-        val rv : RecyclerView = findViewById(R.id.rv)
-
-       /* // 아이템들을 넣는다.
-        val items = ArrayList<ContentModel>()
-        items.add(ContentModel("title1","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png","https://philosopher-chan.tistory.com/1235"))
+            .setValue(ContentModel("title4","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcOYyBM%2Fbtq67Or43WW%2F17lZ3tKajnNwGPSCLtfnE1%2Fimg.png","https://philosopher-chan.tistory.com/1238"))
+*/
+        /*items.add(ContentModel("title1","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FblYPPY%2Fbtq66v0S4wu%2FRmuhpkXUO4FOcrlOmVG4G1%2Fimg.png","https://philosopher-chan.tistory.com/1235"))
         items.add(ContentModel("title2","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FznKK4%2Fbtq665AUWem%2FRUawPn5Wwb4cQ8BetEwN40%2Fimg.png","https://philosopher-chan.tistory.com/1236"))
         items.add(ContentModel("title3","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fbtig9C%2Fbtq65UGxyWI%2FPRBIGUKJ4rjMkI7KTGrxtK%2Fimg.png","https://philosopher-chan.tistory.com/1237"))
         items.add(ContentModel("title4","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FcOYyBM%2Fbtq67Or43WW%2F17lZ3tKajnNwGPSCLtfnE1%2Fimg.png","https://philosopher-chan.tistory.com/1238"))
@@ -52,22 +94,5 @@ class ContentsListActivity : AppCompatActivity() {
         items.add(ContentModel("title13","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbdIKDG%2Fbtq64M96JFa%2FKcJiYgKuwKuP3fIyviXm90%2Fimg.png","https://philosopher-chan.tistory.com/1247"))
         items.add(ContentModel("title14","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FFtY3t%2Fbtq65q6P4Zr%2FWe64GM8KzHAlGE3xQ2nDjk%2Fimg.png","https://philosopher-chan.tistory.com/1248"))
         items.add(ContentModel("title15","https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FOtaMq%2Fbtq67OMpk4W%2FH1cd0mda3n2wNWgVL9Dqy0%2Fimg.png","https://philosopher-chan.tistory.com/1249"))*/
-
-
-        // Activity에 있는 rv의 adavpter를 가져와서 연결한다.
-        val rvAdapter = ContentRVAdapter(baseContext, items)
-        rv.adapter = rvAdapter
-
-        rv.layoutManager = GridLayoutManager(this, 2)
-
-        rvAdapter.itemClick = object: ContentRVAdapter.ItemClick{
-            override fun onClick(view: View, position: Int) {
-
-                val intent = Intent(this@ContentsListActivity,ContentShowActivity::class.java)
-                intent.putExtra("url",items[position].webUrl)
-                startActivity(intent)
-
-            }
-        }
     }
 }
