dbfc37ab64f3e7d800dfa7b5f92df50be920a047
ylose <heungeob1003@gmail.com>
2023-09-25 17:15:23 +0900

    게시판 정보를 쓰는 BoardWrite Activity 생성 23.09.25

---
+++ app/src/main/java/com/example/capstonedesign/board/BoardWriteActivity.kt
@@ -0,0 +1,14 @@
+package com.example.capstonedesign.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.capstonedesign.R
+
+class BoardWriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_write)
+
+    }
+}
