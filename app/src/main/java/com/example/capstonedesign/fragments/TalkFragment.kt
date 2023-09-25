75037093939b8d96740879e6c334a319d0f06206
ylose <heungeob1003@gmail.com>
2023-09-25 17:31:57 +0900

    writeBtn을 클릭했을 때 BoardWrite Activity로 화면 이동 기능 구현 23.09.25

--- app/src/main/java/com/example/capstonedesign/fragments/TalkFragment.kt
+++ app/src/main/java/com/example/capstonedesign/fragments/TalkFragment.kt
@@ -1,5 +1,6 @@
 package com.example.capstonedesign.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.capstonedesign.R
+import com.example.capstonedesign.board.BoardWriteActivity
 import com.example.capstonedesign.databinding.FragmentTalkBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -35,6 +37,12 @@ class TalkFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_talk, container, false)
+
+        // TalkFragment의 writeBtn을 클릭하면 BoardWriteActivity로 이동하도록 화면 이동 기능 구현
+        binding.writeBtn.setOnClickListener{
+            val intent = Intent(context, BoardWriteActivity::class.java)
+            startActivity(intent)
+        }
 
         binding.tipTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_talkFragment_to_tipFragment)
