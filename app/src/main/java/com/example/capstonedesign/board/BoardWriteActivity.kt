2da6bd411f3b50103ebb4bf9c15501a7b493c3ba
ylose <heungeob1003@gmail.com>
2023-09-26 11:16:38 +0900

    데이터 바인딩 생성 23.09.26

--- app/src/main/java/com/example/capstonedesign/board/BoardWriteActivity.kt
+++ app/src/main/java/com/example/capstonedesign/board/BoardWriteActivity.kt
@@ -3,12 +3,17 @@ package com.example.capstonedesign.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.capstonedesign.R
+import com.example.capstonedesign.databinding.ActivityBoardWriteBinding
 
 class BoardWriteActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityBoardWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_write)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_board_write)
 
     }
 }
