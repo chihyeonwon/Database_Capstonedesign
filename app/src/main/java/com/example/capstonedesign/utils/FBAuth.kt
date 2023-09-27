1ef48131c38301cb1336b8848b35c6d2e60ec1d4
ylose <heungeob1003@gmail.com>
2023-09-27 15:05:16 +0900

    Firebase Auth에서 로그인한 사용자의 uid를 가져오는 getUid 생성 23.09.27

---
+++ app/src/main/java/com/example/capstonedesign/utils/FBAuth.kt
@@ -0,0 +1,17 @@
+package com.example.capstonedesign.utils
+
+import com.google.firebase.auth.FirebaseAuth
+
+class FBAuth {
+
+    companion object {
+        private lateinit var auth: FirebaseAuth
+
+        fun getUid() : String {
+
+            auth = FirebaseAuth.getInstance()
+
+            return auth.currentUser?.uid.toString()
+        }
+    }
+}
