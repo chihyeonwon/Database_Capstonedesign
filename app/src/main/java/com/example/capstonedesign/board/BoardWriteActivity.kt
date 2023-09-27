8e34db1c45c712fa85622a9aedf70e33b6f0601a
ylose <heungeob1003@gmail.com>
2023-09-27 15:06:04 +0900

    writeBtn을 클릭했을 때 title과 컨텐츠를 각각 변수에 저장 23.09.27

--- app/src/main/java/com/example/capstonedesign/board/BoardWriteActivity.kt
+++ app/src/main/java/com/example/capstonedesign/board/BoardWriteActivity.kt
@@ -1,7 +1,10 @@
 package com.example.capstonedesign.board
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
 import com.example.capstonedesign.R
 import com.example.capstonedesign.databinding.ActivityBoardWriteBinding
 
@@ -11,9 +14,13 @@ class BoardWriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_write)
 
         binding = DataBindingUtil.setContentView(this,R.layout.activity_board_write)
 
+        binding.writeBtn.setOnClickListener {
+
+            val title = binding.titleArea.text.toString()
+            val content = binding.contentArea.text.toString()
+        }
     }
 }
