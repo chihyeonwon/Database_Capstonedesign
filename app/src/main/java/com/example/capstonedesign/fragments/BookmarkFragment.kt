9bfb2fde03d97dc675b2bf4197e9f56e25441f35
ylose <heungeob1003@gmail.com>
2023-09-28 22:38:06 +0900

    카테고리에 있는 데이터를 가져오는 getCategoryData 함수 생성 23.09.28

--- app/src/main/java/com/example/capstonedesign/fragments/BookmarkFragment.kt
+++ app/src/main/java/com/example/capstonedesign/fragments/BookmarkFragment.kt
@@ -1,6 +1,7 @@
 package com.example.capstonedesign.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,13 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.capstonedesign.R
+import com.example.capstonedesign.contentsList.ContentModel
 import com.example.capstonedesign.databinding.FragmentBookmarkBinding
 import com.example.capstonedesign.databinding.FragmentHomeBinding
+import com.example.capstonedesign.utils.FBRef
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,6 +42,13 @@ class BookmarkFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bookmark, container, false)
 
+        // 전체 카테고리에 있는 컨텐츠 데이터 들을 다 가져온다.
+        getCategoryData()
+
+        // 사용자가 북마크한 정보를 다 가져온다.
+
+        // 전체 컨텐츠 중에서, 사용자가 북마크한 정보만 보여준다.
+
         binding.tipTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_bookmarkFragment_to_tipFragment)
         }
@@ -53,8 +66,23 @@ class BookmarkFragment : Fragment() {
         }
 
         return binding.root
-
-
-        // Inflate the layout for this fragment
     }
+
+    private fun getCategoryData() {
+        val postListner = object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // dataModel에 있는 데이터를 하나씩 가져오는 부분
+                for(dataModel in dataSnapshot.children) {
+
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("ContentListActivity", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.category1.addValueEventListener(postListner)
+        FBRef.category2.addValueEventListener(postListner)
+    }
+
 }
