58f109c27d0e1b06a38e9e393c1a029a7b226296
ylose <heungeob1003@gmail.com>
2023-09-29 15:42:48 +0900

    데이터베이스의 boardModel 데이터를 불러오는 getFBBoardData 함수 생성 23.09.29

--- app/src/main/java/com/example/capstonedesign/fragments/TalkFragment.kt
+++ app/src/main/java/com/example/capstonedesign/fragments/TalkFragment.kt
@@ -2,6 +2,7 @@ package com.example.capstonedesign.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,11 @@ import com.example.capstonedesign.board.BoardListLVAdapter
 import com.example.capstonedesign.board.BoardModel
 import com.example.capstonedesign.board.BoardWriteActivity
 import com.example.capstonedesign.databinding.FragmentTalkBinding
+import com.example.capstonedesign.utils.FBAuth
+import com.example.capstonedesign.utils.FBRef
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +31,10 @@ import com.example.capstonedesign.databinding.FragmentTalkBinding
 class TalkFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private lateinit var binding: FragmentTalkBinding
+
+    private val TAG = TalkFragment::class.java.simpleName
+
+    private val boardDataList = mutableListOf<BoardModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,6 +79,32 @@ class TalkFragment : Fragment() {
             it.findNavController().navigate(R.id.action_talkFragment_to_storeFragment)
         }
 
+        getFBBoardData()
+
         return binding.root
+    }
+
+    private fun getFBBoardData() {
+
+        val postListner = object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                // dataModel에 있는 데이터를 하나씩 가져오는 부분
+                for(dataModel in dataSnapshot.children) {
+                    Log.d(TAG, dataModel.toString())
+
+                    val item = dataModel.getValue(BoardModel::class.java)
+                    boardDataList.add(item!!)
+                }
+
+                Log.d(TAG, boardDataList.toString())
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.boardRef.addValueEventListener(postListner)
+
     }
 }
