ddb0514a1530420f3e4dc11c4237a3d680403882
ylose <heungeob1003@gmail.com>
2023-09-29 14:41:34 +0900

    현재 시간을 가져오는 getTime 함수 생성 23.09.29

--- app/src/main/java/com/example/capstonedesign/utils/FBAuth.kt
+++ app/src/main/java/com/example/capstonedesign/utils/FBAuth.kt
@@ -1,6 +1,9 @@
 package com.example.capstonedesign.utils
 
 import com.google.firebase.auth.FirebaseAuth
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class FBAuth {
 
@@ -12,6 +15,16 @@ class FBAuth {
             auth = FirebaseAuth.getInstance()
 
             return auth.currentUser?.uid.toString()
+        }
+
+        // 현재 시간을 가져오는 getTime 함수
+        fun getTime() : String {
+
+            val currentDateTime = Calendar.getInstance().time
+            val dateFormat = SimpleDateFormat("yyyy.MM.dd HH:mm:ss", Locale.KOREA)
+                .format(currentDateTime)
+
+            return dateFormat
         }
     }
 }
