808fc3fddd9211bdffeec77e801ade9ff2eb72dc
ylose <heungeob1003@gmail.com>
2023-09-30 12:08:10 +0900

    BoardInsideActivity 데이터 바인딩 생성 23.09.30

--- app/src/main/java/com/example/capstonedesign/board/BoardInsideActivity.kt
+++ app/src/main/java/com/example/capstonedesign/board/BoardInsideActivity.kt
@@ -3,14 +3,20 @@ package com.example.capstonedesign.board
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import com.example.capstonedesign.R
+import com.example.capstonedesign.databinding.ActivityBoardInsideBinding
 
 class BoardInsideActivity : AppCompatActivity() {
 
     private val TAG = BoardInsideActivity::class.java.simpleName
+
+    private lateinit var binding: ActivityBoardInsideBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_inside)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_inside)
 
         val title = intent.getStringExtra("title").toString()
         val content = intent.getStringExtra("content").toString()
