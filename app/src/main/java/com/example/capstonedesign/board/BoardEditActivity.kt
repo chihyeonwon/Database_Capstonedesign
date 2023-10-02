eafae5f772aca530cf71dbd832ec66c1a14987df
ylose <heungeob1003@gmail.com>
2023-10-02 17:26:30 +0900

    글 수정 기능 구현 23.10.02

--- app/src/main/java/com/example/capstonedesign/board/BoardEditActivity.kt
+++ app/src/main/java/com/example/capstonedesign/board/BoardEditActivity.kt
@@ -1,12 +1,19 @@
 package com.example.capstonedesign.board
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.example.capstonedesign.R
+import com.example.capstonedesign.contentsList.ContentShowActivity
 import com.example.capstonedesign.databinding.ActivityBoardEditBinding
+import com.example.capstonedesign.utils.FBAuth
 import com.example.capstonedesign.utils.FBRef
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.database.DataSnapshot
@@ -14,6 +21,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import java.io.ByteArrayOutputStream
 
 class BoardEditActivity : AppCompatActivity() {
 
@@ -23,6 +31,10 @@ class BoardEditActivity : AppCompatActivity() {
 
     private val TAG = BoardEditActivity::class.java.simpleName
 
+    private lateinit var writerUid : String
+
+    private var isImageUpload = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,6 +42,61 @@ class BoardEditActivity : AppCompatActivity() {
 
         key = intent.getStringExtra("key").toString()
         getBoardData(key)
+        getImageData(key)
+        binding.editBtn.setOnClickListener {
+            editBoardData(key)
+
+            if(isImageUpload) {
+                imageUpload(key)
+            }
+        }
+
+        binding.imageArea.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, 100)
+            isImageUpload = true
+        }
+    }
+
+    private fun editBoardData(key: String) {
+        FBRef.boardRef
+            .child(key) // 랜덤한 값
+            .setValue(
+                BoardModel(binding.titleArea.text.toString(),
+                    binding.contentArea.text.toString(),
+                    writerUid,
+                    FBAuth.getTime()))
+        Toast.makeText(this,"수정완료", Toast.LENGTH_LONG).show()
+        finish()
+    }
+
+    private fun imageUpload(key: String) {
+
+        val storage = Firebase.storage
+        val storageRef = storage.reference
+        val mountainsRef = storageRef.child(key + ".png")
+
+        val imageView = binding.imageArea
+        imageView.isDrawingCacheEnabled = true
+        imageView.buildDrawingCache()
+
+        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+        val baos = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val data = baos.toByteArray()
+
+        val uploadTask = mountainsRef.putBytes(data)
+        uploadTask.addOnFailureListener {
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK && requestCode == 100) {
+            binding.imageArea.setImageURI(data?.data)
+
+        }
     }
     private fun getBoardData(key: String) {
         val postListner = object: ValueEventListener {
@@ -39,6 +106,7 @@ class BoardEditActivity : AppCompatActivity() {
 
                 binding.titleArea.setText(dataModel?.title)
                 binding.contentArea.setText(dataModel?.content)
+                writerUid = dataModel!!.uid
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
@@ -48,5 +116,19 @@ class BoardEditActivity : AppCompatActivity() {
         FBRef.boardRef.child(key).addValueEventListener(postListner)
     }
 
+    private fun getImageData(key: String) {
+        val storageReference = Firebase.storage.reference.child(key + ".png")
 
+        val imageViewFromFB = binding.imageArea
+
+        storageReference.downloadUrl.addOnCompleteListener(OnCompleteListener { task->
+            if(task.isSuccessful) {
+                Glide.with(this)
+                    .load(task.result)
+                    .into(imageViewFromFB)
+            }else {
+
+            }
+        })
+    }
 }
