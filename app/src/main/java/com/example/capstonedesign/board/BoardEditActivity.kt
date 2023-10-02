e9253e34dc149c90418d449706f1d11d38a5f97a
ylose <heungeob1003@gmail.com>
2023-10-02 13:57:54 +0900

    게시물 보기 페이지의 title, content 값을 받아오기 23.10.02

--- app/src/main/java/com/example/capstonedesign/board/BoardEditActivity.kt
+++ app/src/main/java/com/example/capstonedesign/board/BoardEditActivity.kt
@@ -2,17 +2,51 @@ package com.example.capstonedesign.board
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.capstonedesign.R
+import com.example.capstonedesign.databinding.ActivityBoardEditBinding
+import com.example.capstonedesign.utils.FBRef
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 class BoardEditActivity : AppCompatActivity() {
 
     private lateinit var key:String
 
+    private lateinit var binding: ActivityBoardEditBinding
+
+    private val TAG = BoardEditActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_board_edit)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_board_edit)
 
         key = intent.getStringExtra("key").toString()
+        getBoardData(key)
     }
+    private fun getBoardData(key: String) {
+        val postListner = object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                val dataModel = dataSnapshot.getValue(BoardModel::class.java)
+
+                binding.titleArea.setText(dataModel?.title)
+                binding.contentArea.setText(dataModel?.content)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FBRef.boardRef.child(key).addValueEventListener(postListner)
+    }
+
+
 }
