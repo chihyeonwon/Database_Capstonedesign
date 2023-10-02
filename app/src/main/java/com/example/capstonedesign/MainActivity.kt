e8a2d0d59e91d2fed08c00fd300b19f76c759354
ylose <heungeob1003@gmail.com>
2023-10-02 17:37:39 +0900

    settingBtn을 눌렀을 때 SettingActivity 화면 이동 기능 구현 23.10.02

--- app/src/main/java/com/example/capstonedesign/MainActivity.kt
+++ app/src/main/java/com/example/capstonedesign/MainActivity.kt
@@ -5,7 +5,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import com.example.capstonedesign.auth.IntroActivity
+import com.example.capstonedesign.setting.SettingActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -20,5 +22,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<ImageView>(R.id.settingBtn).setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
