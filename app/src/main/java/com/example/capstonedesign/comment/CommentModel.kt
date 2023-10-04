b468643dd6d370688c1182a8b0a7b62f4cf00d95
ylose <heungeob1003@gmail.com>
2023-10-04 23:23:30 +0900

    commentCreatedTime 댓글 생성 데이터 모델 추가23.10.04

--- app/src/main/java/com/example/capstonedesign/comment/CommentModel.kt
+++ app/src/main/java/com/example/capstonedesign/comment/CommentModel.kt
@@ -1,5 +1,6 @@
 package com.example.capstonedesign.comment
 
 data class CommentModel (
-    val commentTitle: String = ""
+    val commentTitle: String = "",
+    val commentCreatedTime: String = ""
 )
